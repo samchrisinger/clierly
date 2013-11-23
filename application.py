751c8613282443e37485f2fac187ccc1c5221fd8
Sam Chrisinger <sam@multivac.(none)>
2013-11-23 08:57:28 -0500

    Adding a route

--- application.py
+++ application.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
