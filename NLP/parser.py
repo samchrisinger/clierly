f86648f4477045f70f552303c555c6a342655761
Sam Chrisinger <sam@multivac.(none)>
2013-11-23 08:48:36 -0500

    Adding rules as a JSON file

--- NLP/parser.py
+++ NLP/parser.py
@@ -1,24 +1,14 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 import re
-
-url = '''/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/'''
-
-keywords = {'Reference':[re.compile(a) for a in ['Section', 'see.*below', 'link']],\
-'Change':[re.compile(a) for a in ['changes?', 'amend(ment)?', 'alteration', 'addition', 'new\sfeatures', 'modify', 'without\snotice']],\
- 'Consent':[re.compile(a) for a in ['consent', 'accept', 'third[\s\-]?party']],\
- 'Violation':[re.compile(a) for a in ['violation','not\spermitted','unauthorized', 'illegal','unlawful','abuse','excessive']],\
- 'Termination':[re.compile(a) for a in ['termination', 'cancellation','loss\sof\sservice', 'suspension', 'discontinue','terminate']],\
- 'Deactivation':[re.compile(a) for a in ['deactivate']],\
- 'Content':[re.compile(a) for a in ['content', 'Content', 'do\snot\sdelete', 'forfeit', 'relinquish','(intellectual)?\sproperty\s(rights)?', 'screen']],\
- 'Risk':[re.compile(a) for a in ['(own\s)?risk', 'cannot\sbe\s(held\s)?responsible', '((can)|(will\s))?not\sbe\s(held)?\sliable', 'loss\sof']],\
- 'Restrictions':[re.compile(a) for a in ['must\sbe']],\
- 'Personal Information':[re.compile(a) for a in ['((full)|(legal)\s)name','(e(\-)?mail\s)?address', 'personal','credit\scard','social\ssecurity(\snumber)?','(ssn)|(SSN)', 'must\senter']],\
- 'Security':[re.compile(a) for a in ['security','password','encrypted']],\
- 'Money':[re.compile(a) for a in ['charges', 'bill((ing)|(ed))?', 'pay(ment)?', '(non\-)?refund(able)?', 'credit((able)|s|(ed))', '(pro(\-)?)?rate(d)?']],\
- 'Arbitration':[re.compile(a) for a in ['attorney','suit','settlement','claim','proceeding']]}
+import json
 
 def main():
     paragraphs = list()
+    with open('rules.json') as rules:
+        rules = rules.read()
+        rules = json.loads(rules)
+    keywords = {category: [re.compile(r) for r in rules[category]] for category in rules.keys()}
+
     with open("examples/github.tos") as file:
         for paragraph in file:
             paragraph = paragraph.split(".")
@@ -33,6 +23,7 @@ def main():
                             categories.add(key)
                 sentence = (sentence, flags, categories)            
                 paragraphs.append(sentence)
+
     tagged = 0.0
     other = 0.0
     for sentence in paragraphs:
