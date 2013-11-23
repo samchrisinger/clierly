247a046a1e4b2cad71f62a1024959b33a3fa0c80
root <root@server.semsphyi.com>
2013-11-23 20:14:28 +0400

    Parser is now actully working

--- parser.py
+++ parser.py
@@ -2,27 +2,26 @@
 import re
 import json
 
-def parse(url):
+def parse(text):
     paragraphs = list()
     with open('rules.json') as rules:
         rules = rules.read()
         rules = json.loads(rules)
     keywords = {category: [re.compile(r) for r in rules[category]] for category in rules.keys()}
-
-    with open("examples/github.tos") as file:
-        for paragraph in file:
-            paragraph = paragraph.split(". ")
-            p_list = list()
-            for sentence in paragraph:
-                sentence = sentence.strip()
-                flags = set()
-                categories = set()
-                for key in keywords.keys():
-                    for keyword in keywords[key]:
-                        if (keyword.search(sentence)):
-                            flags.add(keyword.pattern)
-                            categories.add(key)
-                sentence = (sentence, flags, categories)            
-                p_list.append(sentence)
-            paragraphs.append(p_list)
+    text = text.split('\n')
+    for paragraph in text:
+        paragraph = paragraph.split(". ")
+        p_list = list()
+        for sentence in paragraph:
+            sentence = sentence.strip()
+            flags = set()
+            categories = set()
+            for key in keywords.keys():
+                for keyword in keywords[key]:
+                    if (keyword.search(sentence)):
+                        flags.add(keyword.pattern)
+                        categories.add(key)
+            sentence = (sentence, flags, categories)            
+            p_list.append(sentence)
+        paragraphs.append(p_list)
     return paragraphs
