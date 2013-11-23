b1b9524853bfea5fd2aad30dc62273cbd6df3a50
Fabian <fabian@ubuntu.(none)>
2013-11-23 11:35:49 -0500

    Commented parser file

--- parser.py
+++ parser.py
@@ -1,13 +1,40 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+#------------------------------------------------------------------
+# parser.py
+#
+# Created by: Samuel Chrisinger, Canar Uguz, Fabian von Feilitzsch
+#
+# This is the main script for parsing the Terms of Service 
+# documents for the clierly webapp. 
+#
+# It reads a json file of rules from the file 'rules.json', where 
+# each rule is specified as a category associated with a list of
+# regular expressions.
+#
+# It then recieves a string of input text. The string is broken 
+# into paragraphs, which contain a list of tuples. Each tuple is 
+# of the form (sentence text, set of matcheed keywords, set of 
+# matched categories). 
+#
+# The list of paragraphs is the return value.
+#------------------------------------------------------------------
+
 import re
 import json
 
 def parse(text):
     paragraphs = list()
+
+    # Reads in the rules
     with open('rules.json') as rules:
         rules = rules.read()
         rules = json.loads(rules)
+
+    # Creates a dict of Category:[keyword] pairs, compiling the keyword regexps
     keywords = {category: [re.compile(r) for r in rules[category]] for category in rules.keys()}
+
+    # Matches substrings in each sentence to the keyword regular expressions
     text = text.split('\n')
     for paragraph in text:
         paragraph = paragraph.split(". ")
