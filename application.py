224e23a6b7a6b6c7f8e4299e2f1f7018e53d345a
caneruguz <caner@Caners-MacBook-Air.local>
2014-01-27 23:23:38 -0500

    Page design changes and removal of unused elements

--- application.py
+++ application.py
@@ -13,15 +13,25 @@ app.config['DEBUG'] = True
 def map_text(paragraph):
     return [{'text':sentence[0],'categories':list(sentence[1]),'message':sentence[2]} for sentence in paragraph]
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
- 
+    
+@app.route('/map')
+def map():
+    return render_template('map.html') 
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+        
 @app.route('/parser', methods=['POST'])
 def process():
     text = parser.parse(request.form['text'])
     text = {'score': text[0], 'text': map(map_text, text[1])}
     return json.dumps(text)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
